d12787a74e2e82c0f647b19b08e5cf4df6a7d997
Andrew Wanex <awanex2705@gmail.com>
2019-03-18 15:41:19 -0700

    feat: add router paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { CategoryComponent } from './components/category/category.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+const routes: Routes = [
+  { path: 'home',     component: HomeComponent       },
+  { path: 'category', component: CategoryComponent   },
+  { path: 'profile',  component: ProfileComponent    },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
