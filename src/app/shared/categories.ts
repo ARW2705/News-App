5ed4e9e198e7a44e9bf71c04d4a29582028138a8
Andrew Wanex <awanex2705@gmail.com>
2019-03-18 15:47:33 -0700

    feat: add predefined collections

---
+++ src/app/shared/categories.ts
@@ -0,0 +1,9 @@
+export const categories = [
+  'Business',
+  'Entertainment',
+  'General',
+  'Health',
+  'Science',
+  'Sports',
+  'Technology'
+];
---
+++ src/app/shared/languages.ts
@@ -0,0 +1,54 @@
+export const Languages = [
+  {
+    name: 'English',
+    code: 'en'
+  },
+  {
+    name: 'العربية',
+    code: 'ar'
+  },
+  {
+    name: 'Deutsch',
+    code: 'de'
+  },
+  {
+    name: 'Español',
+    code: 'es'
+  },
+  {
+    name: 'Français',
+    code: 'fr'
+  },
+  {
+    name: 'עברית',
+    code: 'he'
+  },
+  {
+    name: 'Italiano',
+    code: 'it'
+  },
+  {
+    name: 'Nederlands',
+    code: 'nl'
+  },
+  {
+    name: 'Norsk',
+    code: 'no'
+  },
+  {
+    name: 'Português',
+    code: 'pt'
+  },
+  {
+    name: 'русский',
+    code: 'ru'
+  },
+  {
+    name: 'Davvisámegiella',
+    code: 'se'
+  },
+  {
+    name: '中文 (Zhōngwén)',
+    code: 'zh'
+  }
+];
