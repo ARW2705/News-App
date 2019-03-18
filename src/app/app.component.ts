40c1f43b776b74c9349be262cd68c89604d7a0de
Andrew Wanex <awanex2705@gmail.com>
2019-03-18 15:39:36 -0700

    feat: add svg icon imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NewsApp';
+
+  constructor(iconRegistry: MatIconRegistry,
+    sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('left-nav-arrow', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/left-nav-arrow.svg'));
+    iconRegistry.addSvgIcon('right-nav-arrow', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/right-nav-arrow.svg'));
+    iconRegistry.addSvgIcon('alert-base', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/alert-base.svg'));
+    iconRegistry.addSvgIcon('alert-disabled', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/alert-disabled.svg'));
+    iconRegistry.addSvgIcon('alert-on', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/alert-on.svg'));
+    iconRegistry.addSvgIcon('category', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/category.svg'));
+    iconRegistry.addSvgIcon('home', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/home.svg'));
+    iconRegistry.addSvgIcon('person', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/person.svg'));
+    iconRegistry.addSvgIcon('person-add', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/person-add.svg'));
+    iconRegistry.addSvgIcon('settings', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/settings.svg'));
+    iconRegistry.addSvgIcon('menu', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/menu.svg'));
+    iconRegistry.addSvgIcon('cancel', sanitizer.bypassSecurityTrustResourceUrl('assets/images/icons/cancel.svg'));
+  }
 }
