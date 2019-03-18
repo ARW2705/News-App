57553840bca73948ed57ad3ba3079522b33dc790
Andrew Wanex <awanex2705@gmail.com>
2019-03-18 15:55:46 -0700

    feat: add http interceptors for authed/not-authed requests

---
+++ src/app/services/interceptor.service.ts
@@ -0,0 +1,51 @@
+import { Injectable, Injector } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(private inj: Injector) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const userService = this.inj.get(UserService);
+    const authToken = userService.getToken();
+    const authReq = req.clone({headers: req.headers.set('Authorization', `bearer ${authToken}`)});
+    return next.handle(authReq);
+  }
+}
+
+@Injectable()
+export class UnauthInterceptorService implements HttpInterceptor {
+  constructor(private inj: Injector) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const userService = this.inj.get(UserService);
+    const authToken = userService.getToken();
+
+    return next
+      .handle(req)
+      .pipe(tap((event: HttpEvent<any>) => {
+        // do nothing
+      }, (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status == 401 && authToken) {
+            console.log('Unauthorized', err);
+            userService.checkJWTtoken();
+          }
+        }
+      }));
+  }
+}
