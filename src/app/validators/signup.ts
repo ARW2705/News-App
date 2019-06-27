71b312129ae58b947be83aa8ec7161d60cbaf54e
Andrew Wanex <awanex2705@gmail.com>
2019-06-27 11:56:29 -0700

    chore: add documentation

--- src/app/validators/signup.ts
+++ src/app/validators/signup.ts
@@ -2,12 +2,14 @@ import { AbstractControl, FormGroup, ValidatorFn } from '@angular/forms';
 
 import { passwordPattern } from '../shared/password-pattern';
 
+// Form validator - password must match regex pattern
 export function PasswordPattern(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     return passwordPattern.test(control.value) ? null: {passwordInvalid: true};
   }
 }
 
+// Form validator - password and confirmation must match
 export function PasswordMatch(): ValidatorFn {
   return (group: FormGroup): {[key: string]: any} | null => {
     const password = group.get('password');
